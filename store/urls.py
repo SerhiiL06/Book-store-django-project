f25cc36968ee5ee56a81fce022561749c0062f1a
SerhiiL06 <142268536+SerhiiL06@users.noreply.github.com>
2023-10-03 23:39:04 +0300

    add favorite list functional

--- store/urls.py
+++ store/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
         views.StoreIndexView.as_view(),
         name="category",
     ),
+    path("add_to_fav_list/<int:book_id>/", views.add_to_fav, name="add"),
+    path("favorite_list/", views.FavorireListView.as_view(), name="favorite"),
+    path("favorite_list/delete/", views.delete_favorite_list, name="delete-all"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,14 @@
 from typing import Any
 
-from django.db.models.query import QuerySet
+
 from .models import Book, Category
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import ListView, DetailView, View
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 
 
 class StoreIndexView(ListView):
@@ -20,6 +26,7 @@ class StoreIndexView(ListView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         category_slug = self.kwargs.get("category_slug")
+        context["fav_list"] = self.request.session.get("fav_list")
         if category_slug:
             context["categor"] = Category.objects.get(slug=category_slug)
         return context
@@ -28,3 +35,47 @@ class StoreIndexView(ListView):
 class BookDetailView(DetailView):
     template_name = "store/product-info.html"
     model = Book
+
+
+def add_to_fav(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    fav_list = request.session.get("fav_list")
+
+    if not fav_list:
+        request["fav_list"] = list()
+
+    request.session["fav_list"].append(book_id)
+
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+
+def add_to_fav(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    fav_list = request.session.get("fav_list", [])
+
+    if book_id not in fav_list:
+        fav_list.append(book_id)
+    else:
+        fav_list.remove(book_id)
+
+    request.session["fav_list"] = fav_list
+
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+
+def delete_favorite_list(request):
+    fav_list = request.session.get("fav_list")
+    fav_list.clear()
+    request.session["fav_list"] = fav_list
+
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+
+class FavorireListView(View):
+    def get(self, request):
+        fav_list = request.session.get("fav_list", [])
+        fav_books = Book.objects.filter(id__in=fav_list)
+
+        return render(request, "store/favorite-list.html", {"fav_list": fav_books})
